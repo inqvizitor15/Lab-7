bb97721ffb5f9cba7f6ad0d2a1c61082af000fa6
Gwynbleidd <ushanov1000@gmail.com>
2025-02-13 22:20:42 +0300

    up1

--- weather_mine.py
+++ weather_mine.py
@@ -1,10 +1,11 @@
 import requests
+from pprint import pprint
 import json
-# Ваш API-ключ от OpenWeatherMap
-API_KEY = "d4b8729805537956569f2a16003f293c"
+
+API_KEY = input('Введите ваш API-ключ, который можено посмотреть в личном кабинете OpenWeatherMap: ')
 
 # Название города
-city_name = "Moscow"
+city_name = input('Введите название города, для которого вы хотите узнать погоду (на Англ. яз.): ')
 
 # URL для запроса к API OpenWeatherMap
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric"
@@ -16,23 +17,21 @@ response = requests.get(url)
 if response.status_code == 200:
     # Парсинг JSON-ответа
     data = response.json()
-    data_json = json.dumps(data, indent=4)
+    # print(data)
+    # pprint(data, indent=4)
 
-    print(type(data_json))
-    print(data_json)
-    # weather = data_json["weather"]["description"]
-#     # Извлечение данных о погоде
-#     weather = data["weather"][0]["description"]
-#     temperature = data["main"]["temp"]
-#     humidity = data["main"]["humidity"]
-#     pressure = data["main"]["pressure"]
-#
-#     # Вывод результатов
+    # Извлечение данных о погоде
+    weather = data["weather"][0]["description"]
+    temperature = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    pressure = data["main"]["pressure"]
+
+     # Вывод результатов
     print(f"Погода в городе {city_name}:")
-#     print(f"Температура: {temperature}°C")
-#     print(f"Влажность: {humidity}%")
-#     print(f"Давление: {pressure} hPa")
+    print(f"Температура: {temperature}°C")
+    print(f"Влажность: {humidity}%")
+    print(f"Давление: {pressure} hPa")
     print(f"Описание: {weather.capitalize()}")
-# else:
-#     print(f"Ошибка: {response.status_code}")
-#     print(response.json())  # Вывод сообщения об ошибке
+else:
+    print(f"Ошибка: {response.status_code}")
+    print(response.json())  # Вывод сообщения об ошибке
